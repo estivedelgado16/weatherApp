a18b0895ade62979ab042c5647896acd80015c3a
estivedelgado16 <138074670+estivedelgado16@users.noreply.github.com>
2023-12-21 00:21:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function fetchWeather() {
     const weatherInfoContainer = document.getElementById("weather-info");
 
     try {
-        const apiKey = 'd44bce85822d085e12abe8eba7e3380';
+        const apiKey = '3d44bce85822d085e12abe8eba7e3380';
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
         
         if (!response.ok) {
@@ -16,14 +16,4 @@ async function fetchWeather() {
         console.error(error);
         weatherInfoContainer.textContent = "Error fetching weather data. Please try again.";
     }
-}
-
-function generateWeatherText(weatherData) {
-    return `
-        Weather Information for ${weatherData.name}, ${weatherData.sys.country}
-        Temperature: ${weatherData.main.temp}°C
-        Humidity: ${weatherData.main.humidity}%
-        Pressure: ${weatherData.main.pressure} hPa
-        Wind Speed: ${weatherData.wind.speed} m/s
-    `;
 }
